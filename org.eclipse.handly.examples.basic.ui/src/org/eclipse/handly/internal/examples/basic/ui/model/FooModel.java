f7bf8720d5887ee07fcc3c1cf392c51cd6b1c7f2
Vladimir Piskarev <pisv@1c.ru>
2014-12-09 16:07:22 +0300

    Bug 454552 - Extra fields in elements should be accounted for in both equals and hashCode
    FooDef #hashCode now takes into account the extra field `arity`.
    
    Also, FooModel #equals doesn't call super anymore to be consistent with #hashCode.

--- org.eclipse.handly.examples.basic.ui/src/org/eclipse/handly/internal/examples/basic/ui/model/FooModel.java
+++ org.eclipse.handly.examples.basic.ui/src/org/eclipse/handly/internal/examples/basic/ui/model/FooModel.java
@@ -114,7 +114,7 @@ public class FooModel
     {
         if (this == obj)
             return true;
-        if (!super.equals(obj))
+        if (obj == null)
             return false;
         if (getClass() != obj.getClass())
             return false;
