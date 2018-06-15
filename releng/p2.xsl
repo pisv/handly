b32650e85469e272857b3e049e32b9f946af9d78
Vladimir Piskarev <pisv@1c.ru>
2018-06-15 17:28:04 +0300

    Bug 535864 - Improve promotion script

---
+++ releng/p2.xsl
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+   Copyright (c) 2018 1C-Soft LLC.
+
+   This program and the accompanying materials are made available under
+   the terms of the Eclipse Public License 2.0 which is available at
+   https://www.eclipse.org/legal/epl-2.0/
+
+   SPDX-License-Identifier: EPL-2.0
+
+   Contributors:
+       Vladimir Piskarev (1C) - initial API and implementation
+-->
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+  <xsl:output method="xml" encoding="UTF-8" indent="yes"/>
+  <xsl:strip-space elements="*"/>
+
+  <xsl:param name="mirrorsURL"/>
+  <xsl:param name="statsId"/>
+
+  <!-- add missing newline -->
+  <xsl:template match="repository">
+    <xsl:text>&#10;</xsl:text>
+    <xsl:copy>
+      <xsl:apply-templates select="node() | @*"/>
+    </xsl:copy>
+  </xsl:template>
+
+  <!-- add p2.mirrorsURL and p2.statsURI properties -->
+  <xsl:template match="repository/properties">
+    <properties size="{@size+2}">
+      <xsl:copy-of select="property"/>
+      <property name="p2.mirrorsURL" value="{$mirrorsURL}"/>
+      <property name="p2.statsURI" value="http://download.eclipse.org/stats/handly"/>
+    </properties>
+  </xsl:template>
+
+  <!-- add download.stats property -->
+  <xsl:template match="repository/artifacts/artifact[
+    @classifier='osgi.bundle' and (@id='org.eclipse.handly' or
+    @id='org.eclipse.handly.xtext.ui' or @id='org.eclipse.handly.examples')
+    ]/properties">
+    <properties size="{@size+1}">
+      <xsl:copy-of select="property"/>
+      <property name="download.stats" value="{../@id}-{$statsId}"/>
+    </properties>
+  </xsl:template>
+
+  <!-- copy everything else -->
+  <xsl:template match="node() | @*">
+    <xsl:copy>
+      <xsl:apply-templates select="node() | @*"/>
+    </xsl:copy>
+  </xsl:template>
+
+</xsl:stylesheet>
--- releng/promote.sh
+++ releng/promote.sh
@@ -16,7 +16,7 @@
 
 # Handly promotion script
 #
-# This script downloads the specified build from Handly HIPP and
+# This script downloads the specified build from Handly JIPP and
 # prepares build artifacts for distribution from Eclipse download facility.
 # In particular, it adds additional p2 properties such as p2.mirrorsURL
 # and p2.statsURI. The result can be found in the <build-tag> subdirectory
@@ -24,18 +24,12 @@
 # This subdirectory can then be uploaded to Handly downloads area on
 # build.eclipse.org.
 #
-# To add p2.mirrorsURL and p2.statsURI properties this script uses
-# WTP releng tool addRepoProperties.
+# To add p2.mirrorsURL and p2.statsURI properties to the artifact repository
+# this script uses xsltproc and the p2.xsl file co-located with this file.
 #
 # Requirements:
 # * JAVA_HOME properly set
-# * ECLIPSE_HOME pointing to a directory with an Eclipse native executable launcher.
-#   Make shure that Eclipse installation contains org.eclipse.wtp.releng.tools.feature
-#   (the feature can be installed from http://download.eclipse.org/webtools/releng/repository/).
-#   If this variable is not set, and the working directory has a subdirectory named
-#   "eclipse" (or "Eclipse.app" on macOS), ECLIPSE_HOME will be set to "./eclipse"
-#   (or "./Eclipse.app/Contents/MacOS" on macOS) by default
-# * pwd, curl and unzip utilities
+# * pwd, curl, xsltproc, xz, zip and unzip utilities
 #
 # Optional:
 # * DOWNLOADS_AREA may specify a directory name so that the path
@@ -45,41 +39,30 @@
 #   If this variable is not set, "releases" is assumed.
 #   Use "drops" for non-release builds.
 #
-# Usage: ./promote.sh <hudson-job-name> <hudson-build-number> [<build-tag>]
+# Usage: ./promote.sh <ci-job-name> <ci-build-number> [<build-tag>]
 #
 # The optional <build-tag> specifies a label under which the build will be
 # published, e.g. 0.1, 0.1RC1, 0.1.v20141002-1000. If this parameter is omitted,
-# the value from the VERSION file of the specified Hudson build will be used.
+# the value from the VERSION file of the specified CI build will be used.
 
 #
 # Parsing args
 #
 
-USAGE="Usage: ./promote.sh <hudson-job-name> <hudson-build-number> [<build-tag>]"
-
-if [[ "$ECLIPSE_HOME" == "" ]]; then
-    if [[ -d "./eclipse" ]]; then
-        ECLIPSE_HOME="./eclipse"
-    elif [[ -d "./Eclipse.app" ]]; then
-        ECLIPSE_HOME="./Eclipse.app/Contents/MacOS"
-    else
-        echo "ECLIPSE_HOME must be set"
-        exit -1
-    fi
-fi
+USAGE="Usage: ./promote.sh <ci-job-name> <ci-build-number> [<build-tag>]"
 
 if [[ "$1" != "" ]]; then
     JOB_NAME="$1"
 else
     echo $USAGE
-    exit -1
+    exit 1
 fi
 
 if [[ "$2" != "" ]]; then
     BUILD_ID="$2"
 else
     echo $USAGE
-    exit -1
+    exit 1
 fi
 
 if [[ "$3" != "" ]]; then
@@ -87,26 +70,26 @@ if [[ "$3" != "" ]]; then
     if [[ -d "$3" ]]; then
         echo "Directory '$REPO_VERSION' already exists"
         echo "Remove it or run this script with a different <build-tag>"
-        exit -1
+        exit 1
     fi
 else
     echo "<build-tag> is not specified; will use build VERSION value"
 fi
 
 #
-# Downloading p2 repository from HIPP
+# Downloading p2 repository from JIPP
 #
 
-RESPONSE=$(curl --insecure -s -S -w %{http_code} -o /dev/null https://hudson.eclipse.org/handly/job/$JOB_NAME/$BUILD_ID/artifact/build/VERSION)
+RESPONSE=$(curl --insecure -s -S -w %{http_code} -o /dev/null https://ci.eclipse.org/handly/job/$JOB_NAME/$BUILD_ID/artifact/build/VERSION)
 if [[ "$RESPONSE" != "200" ]]; then
     echo "Cannot download $JOB_NAME build #$BUILD_ID (HTTP $RESPONSE)"
-    exit -1
+    exit 1
 fi
 
-curl --insecure -s -S -O https://hudson.eclipse.org/handly/job/$JOB_NAME/$BUILD_ID/artifact/build/VERSION
+curl --insecure -s -S -O https://ci.eclipse.org/handly/job/$JOB_NAME/$BUILD_ID/artifact/build/VERSION
 if [[ "$?" != "0" ]]; then
     rm -f VERSION
-    exit -1
+    exit 1
 fi
 
 BUILD_VERSION=$(head -n 1 VERSION)
@@ -119,12 +102,12 @@ fi
 if [[ -d "$REPO_VERSION" ]]; then
     echo "Directory '$REPO_VERSION' already exists"
     echo "Remove it or run this script with a different <build-tag>"
-    exit -1
+    exit 1
 fi
 
 mkdir $REPO_VERSION
 if [[ "$?" != "0" ]]; then
-    exit -1
+    exit 1
 fi
 
 REPO_PREFIX=${REPO_PREFIX:-"handly-repository"}
@@ -132,16 +115,16 @@ REPO_FILE=$REPO_VERSION/$REPO_PREFIX-$REPO_VERSION.zip
 
 echo "Downloading p2 repository $BUILD_VERSION..."
 
-curl --insecure -s -S -o $REPO_FILE https://hudson.eclipse.org/handly/job/$JOB_NAME/$BUILD_ID/artifact/build/packages/$REPO_PREFIX-$BUILD_VERSION.zip
+curl --insecure -s -S -o $REPO_FILE https://ci.eclipse.org/handly/job/$JOB_NAME/$BUILD_ID/artifact/build/packages/$REPO_PREFIX-$BUILD_VERSION.zip
 if [[ "$?" != "0" ]]; then
-    exit -1
+    exit 1
 fi
 
 echo "Unzipping p2 repository..."
 
 unzip $REPO_FILE -d $REPO_VERSION/repository > /dev/null
 if [[ "$?" != "0" ]]; then
-    exit -1
+    exit 1
 fi
 
 #
@@ -150,46 +133,54 @@ fi
 
 DOWNLOADS_AREA=${DOWNLOADS_AREA:-"releases"}
 REPOSITORY_PATH="/handly/$DOWNLOADS_AREA/$REPO_VERSION/repository"
-P2_MIRRORS_URL="http://www.eclipse.org/downloads/download.php?file=$REPOSITORY_PATH&format=xml"
-P2_MIRRORS_ARGS=" -DartifactRepoDirectory=$(pwd)/$REPO_VERSION/repository -Dp2MirrorsURL=$P2_MIRRORS_URL"
-
-P2_STATS_URL="http://download.eclipse.org/stats/handly"
-STATS_TRACKED_ARTIFACTS="org.eclipse.handly.ui,org.eclipse.handly.xtext.ui,org.eclipse.handly.examples.basic.ui"
-STATS_ARTIFACTS_SUFFIX="-$REPO_VERSION"
-P2_STATS_ARGS="-Dp2StatsURI=$P2_STATS_URL -DstatsTrackedArtifacts=$STATS_TRACKED_ARTIFACTS -DstatsArtifactsSuffix=$STATS_ARTIFACTS_SUFFIX"
+MIRRORS_URL="http://www.eclipse.org/downloads/download.php?file=$REPOSITORY_PATH&format=xml"
 
 #
 # Updating p2 repository with additional properties
 #
 
-if [[ -f "$ECLIPSE_HOME/eclipsec" ]]; then
-    ECLIPSE_EXECUTABLE="eclipsec"
-elif [[ -f "$ECLIPSE_HOME/eclipse" ]]; then
-    ECLIPSE_EXECUTABLE="eclipse"
-else
-    echo "Cannot find Eclipse executable at $ECLIPSE_HOME"
-    exit -1
-fi
-
 echo "Adding p2 properties..."
 
-$ECLIPSE_HOME/$ECLIPSE_EXECUTABLE -application org.eclipse.wtp.releng.tools.addRepoProperties -nosplash -consoleLog --launcher.suppressErrors -vmargs $P2_MIRRORS_ARGS $P2_STATS_ARGS
+unzip -p $REPO_VERSION/repository/artifacts.jar | xsltproc -stringparam mirrorsURL "$MIRRORS_URL" -stringparam statsId "$REPO_VERSION" p2.xsl - > artifacts.xml
 if [[ "$?" != "0" ]]; then
-    exit -1
+    exit 1
 fi
+
+zip -q $REPO_VERSION/repository/artifacts.jar artifacts.xml
+if [[ "$?" != "0" ]]; then
+    exit 1
+fi
+
+XZ_EXE=$(which xz)
+if [[ "$?" != "0" || -z "${XZ_EXE}" ]]; then
+    echo "Cannot locate xz executable"
+    exit 1
+fi
+
+$XZ_EXE -e --force artifacts.xml
+if [[ "$?" != "0" ]]; then
+    exit 1
+fi
+
+mv -f artifacts.xml.xz $REPO_VERSION/repository/
+if [[ "$?" != "0" ]]; then
+    exit 1
+fi
+
+rm -f artifacts.xml
 
 JAVADOC_PREFIX=${JAVADOC_PREFIX:-"handly-javadoc"}
 JAVADOC_FILE=$REPO_VERSION/$JAVADOC_PREFIX-$REPO_VERSION.zip
 
 #
-# Downloading Javadocs from HIPP
+# Downloading Javadoc from JIPP
 #
 
-echo "Downloading Javadocs $BUILD_VERSION..."
+echo "Downloading Javadoc $BUILD_VERSION..."
 
-curl --insecure -s -S -o $JAVADOC_FILE https://hudson.eclipse.org/handly/job/$JOB_NAME/$BUILD_ID/javadoc/*zip*/javadoc.zip
+curl --insecure -s -S -o $JAVADOC_FILE https://ci.eclipse.org/handly/job/$JOB_NAME/$BUILD_ID/javadoc/*zip*/javadoc.zip
 if [[ "$?" != "0" ]]; then
-    exit -1
+    exit 1
 fi
 
 echo "OK ./$REPO_VERSION"
