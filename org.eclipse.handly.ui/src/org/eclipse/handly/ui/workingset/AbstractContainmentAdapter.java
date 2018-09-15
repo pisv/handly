9a84552f5224c657da3684b3426548cdbc86306f
Vladimir Piskarev <pisv@1c.ru>
2018-09-15 14:49:50 +0300

    Bug 539383 - Rename AbstractContainmentAdapter#check to #isAncestorOf

--- org.eclipse.handly.ui/src/org/eclipse/handly/ui/workingset/AbstractContainmentAdapter.java
+++ org.eclipse.handly.ui/src/org/eclipse/handly/ui/workingset/AbstractContainmentAdapter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2017 1C-Soft LLC and others.
+ * Copyright (c) 2015, 2018 1C-Soft LLC and others.
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which is available at
@@ -24,38 +24,55 @@ import org.eclipse.ui.ide.ResourceUtil;
  * A partial implementation of {@link IContainmentAdapter}
  * for Handly-based models.
  * <p>
- * Each model may provide a containment adapter for its elements (via
- * an adapter factory). That adapter will then be used by the workbench
+ * Containment adapters provide a way to test element containment in a
+ * model-independent way. Each model may contribute a containment adapter
+ * via an adapter factory. The workbench will use the containment adapter
  * to test if a given resource is part of a working set for the model.
- * For instance, it will be used by <code>ResourceWorkingSetFilter</code>.
  * </p>
  *
- * @see #getElementFor(IResource)
+ * @see org.eclipse.ui.ResourceWorkingSetFilter
  */
 public abstract class AbstractContainmentAdapter
     implements IContainmentAdapter
 {
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This implementation returns <code>false</code> if the specified containment
+     * context could not be adapted to an {@link IElement} through the {@link
+     * #getContentAdapter() content adapter}. Otherwise, it attempts to adapt the
+     * given element to an <code>IElement</code> either via the content adapter
+     * or, failing that, by first attempting to adapt it to an {@link IResource}
+     * and then calling {@link #getElementFor(IResource)}. If the given element
+     * could be adapted to an <code>IElement</code>, this implementation delegates
+     * to {@link #contains(IElement, IElement, int)}. If the given element could
+     * be adapted to an <code>IResource</code> but not to an <code>IElement</code>,
+     * this implementation delegates to {@link #contains(IElement, IResource, int)}.
+     * Otherwise, <code>false</code> is returned.
+     * </p>
+     */
     @Override
-    public boolean contains(Object workingSetElement, Object element, int flags)
+    public boolean contains(Object containmentContext, Object element,
+        int flags)
     {
-        IElement hWorkingSetElement = getContentAdapter().adapt(
-            workingSetElement);
-        if (hWorkingSetElement == null || element == null)
+        IElement containmentContextAdapter = getContentAdapter().adapt(
+            containmentContext);
+        if (containmentContextAdapter == null || element == null)
             return false;
 
         IResource resource = null;
-        IElement hElement = getContentAdapter().adapt(element);
-        if (hElement == null)
+        IElement elementAdapter = getContentAdapter().adapt(element);
+        if (elementAdapter == null)
         {
             resource = ResourceUtil.getResource(element);
             if (resource != null)
-                hElement = getElementFor(resource);
+                elementAdapter = getElementFor(resource);
         }
 
-        if (hElement != null)
-            return contains(hWorkingSetElement, hElement, flags);
+        if (elementAdapter != null)
+            return contains(containmentContextAdapter, elementAdapter, flags);
         else if (resource != null)
-            return contains(hWorkingSetElement, resource, flags);
+            return contains(containmentContextAdapter, resource, flags);
         else
             return false;
     }
@@ -72,101 +89,181 @@ public abstract class AbstractContainmentAdapter
 
     /**
      * Returns the content adapter that defines a mapping between elements
-     * of a Handly based model and the working set's content.
+     * of a Handly-based model and the working set's content.
      * <p>
      * Default implementation returns a {@link NullContentAdapter}.
      * Subclasses may override.
      * </p>
      *
-     * @return {@link IContentAdapter} (never <code>null</code>)
+     * @return an {@link IContentAdapter} (never <code>null</code>)
      */
     protected IContentAdapter getContentAdapter()
     {
         return NullContentAdapter.INSTANCE;
     }
 
-    protected boolean contains(IElement workingSetElement, IElement element,
+    /**
+     * Returns whether the given element is considered contained in the given
+     * containment context or if it is the context itself.
+     *
+     * @param containmentContext never <code>null</code>
+     * @param element never <code>null</code>
+     * @param flags one or more of <code>CHECK_CONTEXT</code>,
+     *  <code>CHECK_IF_CHILD</code>, <code>CHECK_IF_ANCESTOR</code>,
+     *  <code>CHECK_IF_DESCENDENT</code> logically ORed together
+     * @return <code>true</code> if the given element is considered contained
+     *  in the given containment context or if it is context itself, and
+     *  <code>false</code> otherwise
+     */
+    protected boolean contains(IElement containmentContext, IElement element,
         int flags)
     {
         if (checkContext(flags) && Elements.equalsAndSameParentChain(
-            workingSetElement, element))
+            containmentContext, element))
         {
             return true;
         }
         if (checkIfChild(flags) && Elements.equalsAndSameParentChain(
-            workingSetElement, Elements.getParent(element)))
+            containmentContext, Elements.getParent(element)))
         {
             return true;
         }
-        if (checkIfDescendant(flags) && check(workingSetElement, element))
+        if (checkIfDescendant(flags) && isAncestorOf(containmentContext,
+            element))
         {
             return true;
         }
-        if (checkIfAncestor(flags) && check(element, workingSetElement))
+        if (checkIfAncestor(flags) && isAncestorOf(element, containmentContext))
         {
             return true;
         }
         return false;
     }
 
-    protected boolean check(IElement ancestor, IElement descendent)
+    /**
+     * Returns whether the element is an ancestor of the other element.
+     * Does not include the other element itself.
+     *
+     * @param element never <code>null</code>
+     * @param other never <code>null</code>
+     * @return <code>true</code> if the element is an ancestor
+     *  of the other element, and <code>false</code> otherwise
+     */
+    protected boolean isAncestorOf(IElement element, IElement other)
     {
-        return Elements.isAncestorOf(ancestor, Elements.getParent(descendent));
+        return Elements.isAncestorOf(element, Elements.getParent(other));
     }
 
-    protected boolean contains(IElement workingSetElement, IResource resource,
+    /**
+     * Returns whether the given resource is considered contained in the given
+     * containment context or if it corresponds to the context itself.
+     *
+     * @param containmentContext never <code>null</code>
+     * @param resource never <code>null</code>
+     * @param flags one or more of <code>CHECK_CONTEXT</code>,
+     *  <code>CHECK_IF_CHILD</code>, <code>CHECK_IF_ANCESTOR</code>,
+     *  <code>CHECK_IF_DESCENDENT</code> logically ORed together
+     * @return <code>true</code> if the given resource is considered contained
+     *  in the given containment context or if it corresponds to the context
+     *  itself, and <code>false</code> otherwise
+     */
+    protected boolean contains(IElement containmentContext, IResource resource,
         int flags)
     {
-        IResource workingSetResource = Elements.getResource(workingSetElement);
-        if (workingSetResource == null)
+        IResource contextResource = Elements.getResource(containmentContext);
+        if (contextResource == null)
             return false;
-        if (checkContext(flags) && workingSetResource.equals(resource))
+        if (checkContext(flags) && contextResource.equals(resource))
         {
             return true;
         }
-        if (checkIfChild(flags) && workingSetResource.equals(
-            resource.getParent()))
+        if (checkIfChild(flags) && contextResource.equals(resource.getParent()))
         {
             return true;
         }
-        if (checkIfDescendant(flags) && check(workingSetResource, resource))
+        if (checkIfDescendant(flags) && isAncestorOf(contextResource, resource))
         {
             return true;
         }
-        if (checkIfAncestor(flags) && check(resource, workingSetResource))
+        if (checkIfAncestor(flags) && isAncestorOf(resource, contextResource))
         {
             return true;
         }
         return false;
     }
 
-    protected boolean check(IResource ancestor, IResource descendent)
+    /**
+     * Returns whether the resource is an ancestor of the other resource.
+     * Does not include the other resource itself.
+     *
+     * @param resource never <code>null</code>
+     * @param other never <code>null</code>
+     * @return <code>true</code> if the resource is an ancestor
+     *  of the other resource, and <code>false</code> otherwise
+     */
+    protected boolean isAncestorOf(IResource resource, IResource other)
     {
-        descendent = descendent.getParent();
-        while (descendent != null)
+        other = other.getParent();
+        while (other != null)
         {
-            if (ancestor.equals(descendent))
+            if (resource.equals(other))
                 return true;
-            descendent = descendent.getParent();
+            other = other.getParent();
         }
         return false;
     }
 
+    /**
+     * Returns whether the <code>CHECK_CONTEXT</code> flag is set.
+     *
+     * @param flags one or more of <code>CHECK_CONTEXT</code>,
+     *  <code>CHECK_IF_CHILD</code>, <code>CHECK_IF_ANCESTOR</code>,
+     *  <code>CHECK_IF_DESCENDENT</code> logically ORed together
+     * @return <code>true</code> if the <code>CHECK_CONTEXT</code> flag is set,
+     *  and <code>false</code> otherwise
+     */
     protected static boolean checkContext(int flags)
     {
         return (flags & CHECK_CONTEXT) != 0;
     }
 
+    /**
+     * Returns whether the <code>CHECK_IF_CHILD</code> flag is set.
+     *
+     * @param flags one or more of <code>CHECK_CONTEXT</code>,
+     *  <code>CHECK_IF_CHILD</code>, <code>CHECK_IF_ANCESTOR</code>,
+     *  <code>CHECK_IF_DESCENDENT</code> logically ORed together
+     * @return <code>true</code> if the <code>CHECK_IF_CHILD</code> flag is set,
+     *  and <code>false</code> otherwise
+     */
     protected static boolean checkIfChild(int flags)
     {
         return (flags & CHECK_IF_CHILD) != 0;
     }
 
+    /**
+     * Returns whether the <code>CHECK_IF_ANCESTOR</code> flag is set.
+     *
+     * @param flags one or more of <code>CHECK_CONTEXT</code>,
+     *  <code>CHECK_IF_CHILD</code>, <code>CHECK_IF_ANCESTOR</code>,
+     *  <code>CHECK_IF_DESCENDENT</code> logically ORed together
+     * @return <code>true</code> if the <code>CHECK_IF_ANCESTOR</code> flag
+     *  is set, and <code>false</code> otherwise
+     */
     protected static boolean checkIfAncestor(int flags)
     {
         return (flags & CHECK_IF_ANCESTOR) != 0;
     }
 
+    /**
+     * Returns whether the <code>CHECK_IF_DESCENDANT</code> flag is set.
+     *
+     * @param flags one or more of <code>CHECK_CONTEXT</code>,
+     *  <code>CHECK_IF_CHILD</code>, <code>CHECK_IF_ANCESTOR</code>,
+     *  <code>CHECK_IF_DESCENDENT</code> logically ORed together
+     * @return <code>true</code> if the <code>CHECK_IF_DESCENDANT</code> flag
+     *  is set, and <code>false</code> otherwise
+     */
     protected static boolean checkIfDescendant(int flags)
     {
         return (flags & CHECK_IF_DESCENDANT) != 0;
