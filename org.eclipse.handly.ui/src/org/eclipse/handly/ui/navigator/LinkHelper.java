10351bebb3643c484376588400101baca9ffb55f
Vladimir Piskarev <pisv@1c.ru>
2022-03-13 21:18:08 +0300

    Bug 579244 - LinkHelper must not activate editor that is already active

--- org.eclipse.handly.ui/src/org/eclipse/handly/ui/navigator/LinkHelper.java
+++ org.eclipse.handly.ui/src/org/eclipse/handly/ui/navigator/LinkHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2018 1C-Soft LLC and others.
+ * Copyright (c) 2014, 2022 1C-Soft LLC and others.
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which is available at
@@ -106,7 +106,7 @@ public abstract class LinkHelper
         if (reference != null)
         {
             IEditorPart editor = reference.getEditor(true);
-            if (editor != null)
+            if (editor != null && editor != page.getActiveEditor())
             {
                 page.bringToTop(editor);
                 editorUtility.revealElement(editor, element);
